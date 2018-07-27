a60b9253c20c83e5ea11fa576f3ffe6004d5e3ce
sobrienpdx <shaunna.obrien@gmail.com>
2018-07-26 20:57:54 -0700

    made createInstance() function to use at events

--- toDoList.js
+++ toDoList.js
@@ -48,57 +48,20 @@ class Task {
   }
 }
 
-// function addItem() {
-//     //Create an ELEMENT
-//     let itemToAdd = document.createElement("li");
-//     //Give the ELEMENT text
-//     let text = document.createTextNode(document.getElementById('item-to-add').value);
-//     console.log(text)
-//     itemToAdd.appendChild(text);
-//     //Make a "done" button, give that text, stick it on the ELEMENT
-//     markDoneBtn = document.createElement('button')
-//     markDoneBtn.innerText = '\u2714'
-//     itemToAdd.appendChild(markDoneBtn)
-//     //Make the 'done' button click add a class to ELEMENT for CS styling purposes (MAKE THE TOGGLE SETTING IN toDoItems SET ITSELF ACCORDINGLY!!!!)
-//     markDoneBtn.addEventListener('click', function(evt){
-//       itemToAdd.classList.toggle('completed')
-//     })
-//     //Make a "remove" button, give that text, stick it on the ELEMENT
-//     removeBtn = document.createElement('button')
-//     removeBtn.innerText = '\u2717'
-//     itemToAdd.appendChild(removeBtn)
-//     //Make the 'remove' button click erase ELEMENT (MAKE SURE TO REMOVE THE ELEMENT FROM toDoItems AS WELL!!!!!)
-//     removeBtn.addEventListener('click', function(evt){
-//       document.getElementById('to-do').removeChild(itemToAdd)
-//     })
-//     // Add  ELEMENT to the appropriate parent in html
-//     document.getElementById("to-do").appendChild(itemToAdd);
-//
-//     // store the text and "done" button toggle state of the ELEMENT in a list that can be saved in localStorage
-//     // toDoItems.push(document.getElementById('item-to-add').value)
-//     // console.log(toDoItems)
-//
-//
-//     toDoItems.push( { "text" : document.getElementById('item-to-add').value, "toggle" : "false"} );
-//     console.log(toDoItems)
-//
-// }
-
-
-document.getElementById("add").addEventListener("click", function(){
-  // addItem()
-
-  // button event creates a new instance of Task class
+function createInstance(){
   listItem = new Task(document.getElementById('item-to-add').value, false)
   listItem.display()
   listItem.keepTrack()
   console.log(listContainingAllTheToDoItemsToSave)
   document.getElementById("item-to-add").value=""
+}
+
+document.getElementById("add").addEventListener("click", function(){
+  createInstance()
 });
 
-// document.getElementById("item-to-add").addEventListener("keypress", function(e) {
-//     if (e.keyCode == 13) {
-//       addItem()
-//       document.getElementById("item-to-add").value=""
-//     }
-// });
+document.getElementById("item-to-add").addEventListener("keypress", function(e) {
+    if (e.keyCode == 13) {
+      createInstance()
+    }
+});
