5c8e3d5d8866ab52a6718f910f9b8212b2abc080
sobrienpdx <shaunna.obrien@gmail.com>
2018-08-02 11:42:09 -0700

    re-roll works and tracks total correctly

--- visualDice.js
+++ visualDice.js
@@ -1,21 +1,41 @@
 let total = 0
+let numsToTotal = []
 function getNumber() {
   return Math.floor(Math.random() * Math.floor(6)+1);
 }
 
 function createDie(){
   let die = document.createElement('h1')
-  die.innerText = getNumber()
+  let number = getNumber()
+  die.innerText = number
   document.getElementById('dice').appendChild(die)
-  return die.innerText
+  numsToTotal.push(number)
+  console.log(numsToTotal)
+
+  //add re-roll button
+  let reRoll = document.createElement('button')
+  reRoll.innerText = 're-roll'
+  die.appendChild(reRoll)
+  reRoll.addEventListener('click', (evt) => {
+    document.getElementById('dice').removeChild(die)
+    numsToTotal.push(- number)
+    createDie()
+    printTotal()
+  })
+}
+
+function printTotal(){
+  let total = numsToTotal.reduce((a, b) => a + b, 0)
+  document.getElementById("total").innerText=(`Your total is ${total}`)
 }
 
 document.getElementById("roll").addEventListener("click", function(){
   let howMany = document.getElementById('howMany').value
   while (howMany >0){
     let number = parseInt(createDie())
-    total += number
     howMany --
-    document.getElementById("total").innerText=(`Your total is ${total}`)
+    printTotal()
+    document.getElementById("roll").innerText=('Add more')
+
   }
 })
